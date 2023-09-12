248f8fc0e970f0f75d095393df3f29cb0323bfad
Sergei <radchenkovsa@yandex.ru>
2023-09-12 23:13:00 +0300

    Case5

---
+++ case5/Program.cs
@@ -0,0 +1,66 @@
+﻿int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] Creat2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+
+void Fill2DArray(int[,] array, int k)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = (i * array.GetLength(1) * k + 1) + k * j;
+        }
+    }
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");       
+        Console.WriteLine();
+    }
+}
+
+int[] FindNumberByPosition(int [,] matrix, int rowPosition, int columnPosition)
+{
+    int[] array = new int[3];
+    
+    if (rowPosition > matrix.GetLength(0) || columnPosition > matrix.GetLength(1))
+     {   return array = new int[3] {0, 0, 0}; }
+    else
+    {
+        return array = new int[3] {rowPosition, columnPosition, matrix[rowPosition - 1, columnPosition - 1]};
+    }
+     
+}
+
+void PrintCheckIfError(int[] results, int X, int Y)
+{
+   if (results[0] == 0)
+        Console.WriteLine("There is no such index");
+    else 
+        Console.WriteLine($"The number in [{X}, {Y}] is {results[2]}");
+}
+
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+
+int[,] myArray = Creat2DArray(rows, columns);
+
+int plus = InputNum("Введите число, на которое увеличивается каждый новый элемент: ");
+int x = InputNum("Введите значение X: ");
+int y = InputNum("Введите значение Y: ");
+
+Fill2DArray(myArray, plus);
+Print2DArray(myArray);
+int[] result = FindNumberByPosition(myArray, x, y);
+PrintCheckIfError(result, x, y);
---
+++ case6/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+public class Answer {
+    public static void PrintArray (int [,] matrix)
+    {
+      // Введите свое решение ниже
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {  Console.Write($"{matrix[i, j]}\t");  }     
+        Console.WriteLine();
+        }
+
+
+    }
+  
+    public static int[,] CreateIncreasingMatrix(int n, int m, int k)
+    {
+      // Введите свое решение ниже
+    int[,] array = new int[n, m];
+      
+      for (int i = 0; i < n; i++)
+      {
+        for (int j = 0; j < m; j++)
+        {
+            array[i, j] = (i * m * k + 1) + k * j;
+        }
+      }
+        return array;
+
+    }
+  
+    static void PrintListAvr (double [] list)
+    {
+      // Введите свое решение ниже
+      Console.WriteLine("The averages in columns are: ");
+      for (int i = 0; i < list.Length; i++)
+      {
+            Console.Write($"{list[i]:f2}\t");
+      }
+      
+    }
+
+    static double [] FindAverageInColumns (int [,] matrix)
+    { 
+      // Введите свое решение ниже
+      double[] array = new double[matrix.GetLength(1)];
+        double a = 0;
+        int b = 0;
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            while (b < matrix.GetLength(0))
+            {
+                a = a + matrix[b, i];
+                b++;
+            }
+            array[i] = a / matrix.GetLength(0);
+            b = 0;
+            a = 0;
+        }
+        return array;
+
+    }
+
+
+    // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int n, m, k;
+
+        if (args.Length >= 3) {
+           n = int.Parse(args[0]);
+           m = int.Parse(args[1]);
+           k = int.Parse(args[2]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+           n = 4;
+           m = 5;
+           k = 3;
+        }
+
+        // Не удаляйте строки ниже
+        int[,] result = CreateIncreasingMatrix(n, m, k);
+        PrintArray(result);
+        PrintListAvr(FindAverageInColumns(result));
+    }
+}
